e7112b8d1df073367576ac5aa87c70595cac61af
Jeremy Brown <jeremynealbrown@gmail.com>
2013-04-27 15:44:51 -0700

    added more tests

--- tests.py
+++ tests.py
@@ -17,7 +17,23 @@ C = [
     ]
 
 class MbyNTests(unittest.TestCase):
+
+    def test_add(self):
+        result = mbyn.add([A, A])
+        self.assertEqual(result, [[4, 6],[6, 6]])
+
+    def test_clone(self):
+        result = mbyn.clone(B)
+        self.assertEquals(result, B)
+
+    def test_directSum(self):
+        result = mbyn.directSum(A, B)
+        self.assertEquals(result, [[2, 3, 0, 0, 0],[3, 3, 0, 0, 0],[0, 0, 2, 5, 6],[0, 0, 2, 3, 2]])
     
+    def test_expo(self):
+        result = mbyn.expo(A, 3)
+        self.assertEquals(result, [[71, 84],[84, 99]])
+
     def test_fill(self):
         result = mbyn.fill(C, 1)
         self.assertEqual(result, [[0, 0, 1], [3, 3, 1]])

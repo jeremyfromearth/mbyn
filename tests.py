61972ddc7dccecb2942dd4f4591b34e8f91d4855
Jeremy Brown <jeremynealbrown@gmail.com>
2013-04-27 15:17:47 -0700

    put matrices in examples in global scope

--- tests.py
+++ tests.py
@@ -1,36 +1,34 @@
 from mbyn import mbyn
 import unittest
 
+A = [
+        [2, 3],
+        [3, 3]
+    ]
+
+B = [
+        [2, 5, 6],
+        [2, 3, 2],
+    ]
+
+C = [
+        [0, 0, 1],
+        [3, 3]
+    ]
 
 class MbyNTests(unittest.TestCase):
+    
+    def test_fill(self):
+        result = mbyn.fill(C, 1)
+        self.assertEqual(result, [[0, 0, 1], [3, 3, 1]])
 
-    def setUp(self):
-        self.A = [
-                    [2, 3],
-                    [3, 3]
-                ]
-
-        self.B = [
-                    [2, 5, 6],
-                    [2, 3, 2],
-                ]
-
-        self.C = [
-                    [0, 0, 1],
-                    [3, 3]
-                ]
-   
     def test_isSqaure(self):
-        result = mbyn.isSquare(self.A)
+        result = mbyn.isSquare(A)
         self.assertTrue(result)
 
     def test_multiply(self):
-        result = mbyn.multiply(self.A, self.B)
+        result = mbyn.multiply(A, B)
         self.assertEqual(result, [[10, 19, 18], [12, 24, 24]])
-
-    def test_fill(self):
-        result = mbyn.fill(self.C, 1)
-        self.assertEqual(result, [[0, 0, 1], [3, 3, 1]])
 
 if __name__ == '__main__':
     unittest.main()
